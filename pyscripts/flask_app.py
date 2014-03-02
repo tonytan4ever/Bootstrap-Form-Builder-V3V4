9eedd81efcbb82bf833672d5e189203825c5ff8d
tonytan4ever <tonytan198211@gmail.com>
2014-03-02 12:30:05 -0500

    Fixing root view

--- pyscripts/flask_app.py
+++ pyscripts/flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, url_for
+from flask import Flask, redirect, url_for
 import os
 
 static_folder=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -6,9 +6,12 @@ static_folder=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 app = Flask(__name__, 
              static_folder=static_folder)
 
+@app.route("/")
+def root_view():
+    return redirect(url_for('static', filename='index.html'))
+
 @app.route("/index")
 def index_view():
-    
     return "<a href=%s>index<a/>" % url_for('static', filename='index.html')
 
 if __name__ == "__main__":
