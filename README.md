9d30b32e3bb06d6494457b319ff1ab652d766216
Isaac Mungai <isaacm@github.com>
2017-09-03 10:27:33 -0400

    Begin work for bootstrap v4

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 Bootstrap-Form-Builder-V3V4
 ==========================
 
-A bootstrap 3.0 form drag and drop builder, implemented in Jquery.  Based on:  https://github.com/minikomi/Bootstrap-Form-Builder
+A bootstrap 4.0 form drag and drop builder, implemented in Jquery.  Based on:  https://github.com/minikomi/Bootstrap-Form-Builder
 
 
 Avaialble Demo at:
 http://formbuilder.tdjangoy.webfactional.com/
 
-pyscripts folder has a simple Flask server to host the DEMO app. 
+pyscripts folder has a simple Flask server to host the DEMO app.
 to Run the Demo locally::
-	
+
 	$ cd <your_repo_path>/pyscripts
-	$ python flask_app.py 
-  
+	$ python flask_app.py
+
 For more detail on Flask, see: http://flask.pocoo.org/
 
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,23 @@
 require.config({
   baseUrl: "assets/js/lib/",
   paths: {
-	'jquery': 'http://code.jquery.com/jquery-2.1.0',
+	'jquery': 'http://code.jquery.com/jquery-3.1.1',
 	'mustache' : "http://rawgithub.com/janl/mustache.js/master/mustache",
-	'bootstrap' : "http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap",
+	'bootstrap' : "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap",
 	'underscore' :  "https://rawgithub.com/jashkenas/underscore/master/underscore",
 	'text'  : "https://rawgithub.com/requirejs/text/latest/text",
 	'app'   : "..",
-	 collections : "../collections", 
-	 data        : "../data", 
-	 models      : "../models", 
-	 helper      : "../helper", 
-	 templates   : "../templates", 
+	 collections : "../collections",
+	 data        : "../data",
+	 models      : "../models",
+	 helper      : "../helper",
+	 templates   : "../templates",
 	 views       : "../views"
   }, shim: {
     'underscore': {
 	      exports: '_'
-	},  
-	  
+	},
+
     'bootstrap': {
       deps: ['jquery'],
       exports: '$.fn.popover'
