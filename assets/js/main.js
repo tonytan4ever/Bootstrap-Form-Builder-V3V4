29d02bd7e5f732eb27706c276e6246cb9762ad26
tonytan4ever <tonytan198211@gmail.com>
2020-02-26 18:08:31 -0500

    pin mustache to 3.1.0 to preserve to_html function

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ require.config({
   baseUrl: "assets/js/lib/",
   paths: {
 	'jquery': 'https://code.jquery.com/jquery-3.2.1',
-	'mustache' : "http://rawgithub.com/janl/mustache.js/master/mustache",
+	'mustache' : "http://rawgithub.com/janl/mustache.js/v3.1.0/mustache",
 	'popper' : "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper",
 	'bootstrap' : "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap",
 	'underscore' :  "https://rawgithub.com/jashkenas/underscore/master/underscore",
